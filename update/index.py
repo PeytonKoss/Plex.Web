e70e04126bfde054b78d71accf26dcbdb2c88fe9
PeytonKoss <peytonkossex@gmail.com>
2024-12-18 08:01:44 -0500

    iOS pin to home functionality

--- update/index.py
+++ update/index.py
@@ -152,6 +152,7 @@ html_content = f'''
 <head>
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="theme-color" content="#121212">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
